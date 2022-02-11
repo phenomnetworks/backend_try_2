c971e66af7686e3f7b787795e9de500c87b0e192
phenomnetworks <91694599+phenomnetworks@users.noreply.github.com>
2022-02-11 23:49:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,18 +10,31 @@ app = Flask(__name__)
 def nameRoute():
 
     global response
-    global response2
 
     if(request.method == 'POST'):
         request_data = request.data
         request_data = json.loads(request_data.decode('utf-8'))
         name = request_data['name']
         response = f'Hallo {name}! Das ist Python'
-        response2 = f'Dein Name ist also {name}, richtig?'
         return " "
 
     else:
-        return jsonify({'name' : response}, {'name2' : response2})        
+        return jsonify({'name' : response}        
 
+@app.route('/name2', methods = ['GET', 'POST'])
+def nameRoute2():
+
+    global response2
+
+    if(request.method == 'POST'):
+        request_data = request.data
+        request_data = json.loads(request_data.decode('utf-8'))
+        name2 = request_data['name2']
+        response2 = f'Dein Name ist also {name2}, richtig?'
+        return " "
+
+    else:
+        return jsonify({'name2' : response2}                        
+                       
 if __name__ == "__main__":
     app.run(debug=True)
