d46e2ce15766e52b4c31d18c7e034b67c1ed6cd6
phenomnetworks <91694599+phenomnetworks@users.noreply.github.com>
2022-02-11 23:39:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 import json
 
 response = ''
+response2 = ''
 
 app = Flask(__name__)
 
@@ -9,16 +10,18 @@ app = Flask(__name__)
 def nameRoute():
 
     global response
+    global response2
 
     if(request.method == 'POST'):
         request_data = request.data
         request_data = json.loads(request_data.decode('utf-8'))
         name = request_data['name']
         response = f'Hallo {name}! Das ist Python'
+        response2 = f'Dein Name ist also {name}, richtig?'
         return " "
 
     else:
-        return jsonify({'name' : response})
+        return jsonify({'name' : response}, {'name' : response2})        
 
 if __name__ == "__main__":
     app.run(debug=True)
