0bf849b4befd5142eeaa40bc6963c6544508538c
phenomnetworks <91694599+phenomnetworks@users.noreply.github.com>
2022-02-12 01:13:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -14,27 +14,11 @@ def nameRoute():
         request_data = request.data
         request_data = json.loads(request_data.decode('utf-8'))
         name = request_data['name']
-        response = f'Hallo {name}! Das ist Python'
+        response = f'{name}'
         return " "
 
     else:
-        return jsonify({'name' : response})   
-
-@app.route('/name2', methods = ['GET', 'POST'])
-def nameRoute2():
-
-    global response
-
-    if(request.method == 'POST'):
-        request_data = request.data
-        request_data = json.loads(request_data.decode('utf-8'))
-        name = request_data['name']
-        response = f'Du bist also{name}, richtig?'
-        return " "
-
-    else:
-        return jsonify({'name2' : response})   
-
+        return jsonify({'name' : 'Hallo' + response} + '!')   
 
 if __name__ == "__main__":
     app.run(debug=True)
